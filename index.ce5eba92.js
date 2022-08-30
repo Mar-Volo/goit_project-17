4f2d4deb54e7278b93cd19eb5630d18c16afdf89
Mar-Volo <Katalizator1992@gmail.com>
2022-08-30 16:08:12 +0000

    Deploying to gh-pages from @ Mar-Volo/goit_project-17@c8318fa900b1550d45b906acd8f9c58a3ae99b95 🚀

---
+++ index.ce5eba92.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalmobBtn:document.querySelector("[open]"),closeModalmobBtn:document.querySelector("[close]"),modalmob:document.querySelector("[modal]")};function e(){o.modalmob.classList.toggle("is-hidden")}o.openModalmobBtn.addEventListener("click",e),o.closeModalmobBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.ce5eba92.js.map
